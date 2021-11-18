ecc8fc231070759b7682605a41e72abbf3c6506e
James Little <littleguy23@gmail.com>
2021-11-17 21:50:23 -0500

    New Insurance Content

--- pages/insurance.js
+++ pages/insurance.js
@@ -27,6 +27,38 @@ plan accordingly.
 If you are having financial difficulties, you may easily arrange a payment
 plan with our office manager. While we cannot waive fees, we can always set
 up a monthly payment plan, should the need arise.
+
+## January 2021 Updates
+
+As you are likely aware, the Affordable Care Act limits patient cost share and
+results in no copayment or deductible for an annual well visit. But, in
+conjunction with the ACA, changes were made to the federal medical billing
+guidelines as of January 1, 2021 which mandated new medical billing regulations.
+The current billing guidelines state that **if a medical problem that is outside
+of normal growth or development is discussed or managed at an annual check up,
+we are required to document and charge for this in addition to the usual charge
+for the well visit itself.**
+
+While well visits generally do not involve copayments or deductible charges,
+with this new federal guideline you may be charged a copayment or deductible
+when a separate medical problem is discussed or managed during a routine
+checkup.
+
+For example, if your child is found to have an ear infection during the routine
+checkup, there must be a separate billing charge for the diagnosis and
+management of the ear infection in addition to the usual charge for the check
+up. Or, if your child has asthma and we review your child’s asthma status and
+review or refill your child’s medications for asthma management, there may be an
+additional charge associated with that visit.
+
+We do not choose how to bill in this way for each visit; we are mandated by
+federal law to follow the new billing guidelines. And, we do not determine the
+amount of your copayment or your deductible. That is determined by your
+insurance company, not by our office.
+
+Please feel free to ask us about these changes or contact our billing department
+if you have any questions about a bill you received from our office. We can be
+reached at 617-232-2811.
 `;
 
 const Insurance = () => (
