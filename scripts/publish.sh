3c54b4f8f4aadf96846b645e6baed73f80c2a222
James Little <littleguy23@gmail.com>
2021-11-24 23:09:48 -0500

    Fix publish script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
-wget https://files.stork-search.net/releases/latest/stork-ubuntu-latest
-chmod +x stork-ubuntu-latest
+wget https://files.stork-search.net/releases/v1.3.0/stork-ubuntu-20-04
+chmod +x stork-ubuntu-20-04
 
 npm run build && npm run export && npm run build
-./stork-ubuntu-latest --build public/search.toml
-
+./stork-ubuntu-20-04 build --input public/search.toml --output public/search-index.st
+rm -rf .next
+rm -rf out
 npm run build && npm run export
