33cbd45be22d6ac70b3e29c22ccc38b2d21f6986
James Little <littleguy23@gmail.com>
2025-05-05 21:44:11 -0400

    Dependency upgrades

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
+import createMDX from '@next/mdx'
 const nextConfig = {
   output: "export",
-  experimental: {
-    mdxRs: true,
-  },
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
 };
 
-const withMDX = require("@next/mdx")();
-module.exports = withMDX(nextConfig);
+const withMDX = createMDX({})
+
+export default withMDX(nextConfig);
